f174ea623efb302abfe260bf7746e7610fc421f7
BillyDyball <47304846+BillyDyball@users.noreply.github.com>
2023-05-24 00:58:36 +0100

    Added firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDTQ2aOGPNICPbZu1r31IOvHTXPC6SZxzE",
+    authDomain: "portfolio-website-f1fba.firebaseapp.com",
+    projectId: "portfolio-website-f1fba",
+    storageBucket: "portfolio-website-f1fba.appspot.com",
+    messagingSenderId: "950139195863",
+    appId: "1:950139195863:web:acac875a38245ba1b0fb92",
+    measurementId: "G-GPVJ6QY4SE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
